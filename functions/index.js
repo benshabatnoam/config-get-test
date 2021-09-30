e170f619e34e164bc545174399567dbbe5adb488
Noam Ben Shabat <n0556771785@gmail.com>
2021-09-30 11:29:24 +0300

    add comments

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require("firebase-functions");
 
+// this line below causing an error (comment it out for successful deploy)
 const username = functions.config().user.name;
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
+    // BUT this line below doesn't cause an error
     const username = functions.config().user.name;
     response.send(`Hello from ${username}!`);
 });
