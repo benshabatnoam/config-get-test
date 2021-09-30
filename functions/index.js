8272da2aafdd3256ef1f7dea09d9bda8e59f358f
Noam Ben Shabat <n0556771785@gmail.com>
2021-09-30 11:27:56 +0300

    remove commented problem

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 const functions = require("firebase-functions");
-// const username = functions.config().user.name;
+
+const username = functions.config().user.name;
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
     const username = functions.config().user.name;
     response.send(`Hello from ${username}!`);
